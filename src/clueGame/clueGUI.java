2272d8bf5466211d10a676933029c1bdde207c65
Daniel Winternitz <dwinternitz@mines.edu>
2018-04-11 14:47:21 -0600

    Added to the GUI window

--- src/clueGame/clueGUI.java
+++ src/clueGame/clueGUI.java
@@ -3,6 +3,8 @@ package clueGame;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +28,7 @@ public class clueGUI extends JPanel{
 		JPanel lowerPanel = new JPanel();
 		lowerPanel.setLayout(new GridLayout(2,2));
 		lowerPanel.add(createTopSubPanel(), BorderLayout.CENTER);
+		lowerPanel.add(createBottomSubPanel(), BorderLayout.CENTER);
 		return lowerPanel;
 	}
 	
@@ -55,11 +58,38 @@ public class clueGUI extends JPanel{
 		return topSubPanel;
 	}
 	
+	public JPanel createBottomSubPanel(){
+		//Creates the upper half of the lower panel
+		JPanel bottomSubPanel = new JPanel();
+		bottomSubPanel.setLayout(new GridLayout(1,3));
+		
+		// makes die box
+		JLabel die = new JLabel("Roll: ");
+		bottomSubPanel.add(die, BorderLayout.WEST);
+		TitledBorder dieTitledBorder = new TitledBorder("Die");
+		die.setBorder(dieTitledBorder);
+		
+		// makes guess box
+		JLabel guess = new JLabel("Guess: ");
+		bottomSubPanel.add(guess, BorderLayout.CENTER);
+		TitledBorder guessTitledBorder = new TitledBorder("Guess");
+		guess.setBorder(guessTitledBorder);
+		
+		// makes Guess Result box
+		JLabel response = new JLabel("Response: ");
+		TitledBorder resultTitledBorder = new TitledBorder("Guess Result");
+		response.setBorder(resultTitledBorder);
+		bottomSubPanel.add(response, BorderLayout.CENTER);
+		
+		
+		return bottomSubPanel;
+	}
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Clue");
-		frame.setSize(500, 500);
+		frame.setSize(800, 800);
 		clueGUI gui = new clueGUI();
 		frame.add(gui, BorderLayout.SOUTH);
 		frame.setVisible(true);
